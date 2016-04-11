b03160a091407620f4b46ef7bb6c67998ce86946
b5614493 <supakan111@outlook.com>
2016-04-11 12:38:13 +0700

    Input All Action Ready Wait Picture

--- game01/core/src/main/java/sut/game01/core/character/Bunny.java
+++ game01/core/src/main/java/sut/game01/core/character/Bunny.java
@@ -17,11 +17,11 @@ public class Bunny {
     private int action = 0;
 
     public enum State {
-        IDLE, LRUN, RRUN
+        RIDLE,LIDLE, LRUN, RRUN,RATK,LATK,LJUM,RJUM,LSLD,RSLD
     };
 
 
-    private State state = State.IDLE; // Start State
+    private State state = State.RIDLE; // Start State
 
     private int e = 0 ; //Time Control
     private int offset = 0 ; //Set Of Picture
@@ -45,15 +45,39 @@ public class Bunny {
         PlayN.keyboard().setListener(new Keyboard.Adapter() {
             @Override
             public void onKeyDown(Keyboard.Event event) {
-                if (event.key() == Key.RIGHT) {
-                    action = 2;
+                if (event.key() == Key.RIGHT && state != State.LJUM && state != State.RJUM) {
+                    action = 4;
                     state = State.RRUN;
                     spriteIndex = -1;
                     e=0;
                 }
-                else  if (event.key() == Key.LEFT){
-                    action = 1;
+                else  if (event.key() == Key.LEFT && state != State.LJUM && state != State.RJUM){
+                    action = 3;
                     state = State.LRUN;
+                    spriteIndex = -1;
+                    e=0;
+                }
+               else if(event.key() == Key.Z && state != State.LJUM && state != State.RJUM){
+                    if(state == State.LIDLE || state == State.LRUN || state == State.LATK){
+                    action = 5;
+                    state = State.LATK;
+                    }
+                    else if(state == State.RIDLE || state == State.RRUN || state == State.RATK){
+                        action = 6;
+                        state = State.RATK;
+                    }
+                    spriteIndex = -1;
+                    e=0;
+                }
+                else if(event.key() == Key.SPACE && state != State.LJUM && state != State.RJUM){
+                    if(state == State.LIDLE || state == State.LRUN || state == State.LATK){
+                        action = 7;
+                        state = State.LJUM;
+                    }
+                    else if(state == State.RIDLE || state == State.RRUN || state == State.RATK){
+                        action = 8;
+                        state = State.RJUM;
+                    }
                     spriteIndex = -1;
                     e=0;
                 }
@@ -64,16 +88,28 @@ public class Bunny {
 
                 if (event.key() == Key.LEFT){
                     action = 0;
-                    state = State.IDLE;
+                    state = State.LIDLE;
                     spriteIndex = -1;
                     e=0;
                 }
 
                else if (event.key() == Key.RIGHT){
-                    action = 0;
-                    state = State.IDLE;
+                    action = 1;
+                    state = State.RIDLE;
                     spriteIndex = -1;
                     e=0;
+                }
+                else if(event.key() == Key.Z){
+                    if(state == State.LATK){
+                    action = 0;
+                    state = State.LIDLE;
+                    }
+                    else if(state == State.RATK){
+                        action = 1;
+                        state = State.RIDLE;
+                    }
+                    spriteIndex = -1;
+                    e = 0;
                 }
             }
 
@@ -92,39 +128,62 @@ public class Bunny {
         if(!hasLoaded) return;
         e = e +delta;
 
-        if(action == 1){
+        if(action == 3){
             state = State.LRUN;
-
-
         }
-        else if(action == 2){
+        else if(action == 4){
             state = State.RRUN;
-
-
         }
         else if(action == 0){
-            state = State.IDLE;
+            state = State.RIDLE;
         }
-    /*    if(e > 150){
-            spriteIndex = offset + ((spriteIndex + 1)%4);
-            sprite.setSprite(spriteIndex);
-            e=0;
-        }*/
+        else if(action == 1){
+            state = State.LIDLE;
+        }
+        else if(action == 5){
+            state = State.LATK;
+        }
+        else if(action == 6){
+            state = State.RATK;
+        }
+
 
         if(e>150){
 
             switch (state){
-                case IDLE: offset = 0;
+                case LIDLE: offset = 0;
                     break;
-                case LRUN: offset = 4 ;
-                    if(spriteIndex == 7){
-                        state =State.IDLE;
+                case RIDLE: offset = 4;
+                    break;
+                case LRUN: offset = 8 ;
+                    if(spriteIndex == 11){
+                        state =State.LIDLE;
                     }
                     break;
-                case RRUN: offset = 8 ;
-                    if(spriteIndex == 10){
-                        state =State.IDLE;
+                case RRUN: offset = 12 ;
+                    if(spriteIndex == 14){
+                        state =State.RIDLE;
                         //action = 0;
+                    }
+                    break;
+                case LATK: offset = 16 ;
+                    if(spriteIndex == 18){
+                        state = State.LIDLE;
+                    }
+                    break;
+                case RATK: offset = 20 ;
+                    if(spriteIndex == 22){
+                        state = State.RIDLE;
+                    }
+                    break;
+                case LJUM: offset = 24 ;
+                    if(spriteIndex == 27){
+                        state = State.LIDLE;
+                    }
+                    break;
+                case RJUM: offset = 28 ;
+                    if(spriteIndex == 31){
+                        state = State.RIDLE;
                     }
                     break;
             }
