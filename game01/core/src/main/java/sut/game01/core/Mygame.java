2a348c95f32f7ad6ff4db510e4c2ecc3b809379f
b5614493 <supakan111@outlook.com>
2016-05-14 17:45:26 +0700

    Add Action with physic

--- game01/core/src/main/java/sut/game01/core/Mygame.java
+++ game01/core/src/main/java/sut/game01/core/Mygame.java
@@ -1,20 +1,25 @@
 package sut.game01.core;
 
+import org.jbox2d.dynamics.Body;
 import playn.core.Game;
 import playn.core.util.Clock;
 import tripleplay.game.ScreenStack;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Mygame extends Game.Default {
     public static final int UPDATE_RATE = 25;
     private ScreenStack ss = new ScreenStack();
     protected final Clock.Source clock = new Clock.Source(UPDATE_RATE);
+   // public static final Map<String, Body> map1 = new HashMap<String, Body>();
 
   public Mygame() {
       super(UPDATE_RATE); // call update every 33ms (30 times per second)
   }
   @Override
   public void init() {
-      ss.push(new Homescreen(ss));
+      ss.push(new TestScreen(ss));
   }
 
   @Override
